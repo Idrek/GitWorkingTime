2b9b07b2978998e234af63ab84b0270cdabbbc8a
David CG <iddarg@gmail.com>
2019-10-02 17:02:07 +0200

    [Test] `buildCommandLogByAuthor` function

--- test/Tests.fs
+++ test/Tests.fs
@@ -1,8 +1,25 @@
-module Tests
+module GitWorkingTimeTest.Tests
 
-open System
+open GitWorkingTime.Program
 open Xunit
 
+// ---------------------------------
+// Tests
+// ---------------------------------
+
+// ---------------------------------
+// buildCommandLogByAuthor
+// ---------------------------------     
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Test buildCommandLogByAuthor function`` () =
+    let options : Options = { Author = "me"; Repository = "../Demo" }
+    Assert.Equal(
+        "cd '../Demo' && git --no-pager log --author='me' --date=iso",
+        buildCommandLogByAuthor options)
+
+    let options : Options = { Author = "he and she"; Repository = "../Demo space" }
+    Assert.Equal(
+        "cd '../Demo space' && git --no-pager log --author='he and she' --date=iso",
+        buildCommandLogByAuthor options)
+
