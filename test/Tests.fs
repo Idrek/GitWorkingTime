85ee5282518d1542264fc99bed104a56b0bc58cf
David CG <iddarg@gmail.com>
2019-10-02 17:17:09 +0200

    [Test] `initHours` function

--- test/Tests.fs
+++ test/Tests.fs
@@ -46,4 +46,16 @@ let ``Test authorHours function`` () =
     Assert.Equal<array<int>>(
         [|17; 12|],
         authorHours authorLog)
+
+// ---------------------------------
+// initHours
+// ---------------------------------     
+
+[<Fact>]
+let ``Test initHours function`` () =
+    Assert.Equal<Map<int, int>>(
+        Map [(0, 0); (1, 0); (2, 0); (3, 0); (4, 0); (5, 0); (6, 0); (7, 0); (8, 0); (9, 0); (10, 0)
+             (11, 0); (12, 0); (13, 0); (14, 0); (15, 0); (16, 0); (17, 0); (18, 0); (19, 0)
+             (20, 0); (21, 0); (22, 0); (23, 0)],
+        initHours ())
         
