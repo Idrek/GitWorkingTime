8a0f62f692ca5ce112ab26a621a015939c53e581
David CG <iddarg@gmail.com>
2019-10-04 11:09:57 +0200

    [Test] Adapt `buildCommandLogByAuthor` function

--- test/Tests.fs
+++ test/Tests.fs
@@ -15,12 +15,12 @@ open Xunit
 let ``Test buildCommandLogByAuthor function`` () =
     let options : Options = { Author = "me"; Repository = "../Demo" }
     Assert.Equal(
-        "cd '../Demo' && git --no-pager log --author='me' --date=iso",
+        "cd '../Demo' && git --no-pager log --author='me' --format='%H %ai'",
         buildCommandLogByAuthor options)
 
     let options : Options = { Author = "he and she"; Repository = "../Demo space" }
     Assert.Equal(
-        "cd '../Demo space' && git --no-pager log --author='he and she' --date=iso",
+        "cd '../Demo space' && git --no-pager log --author='he and she' --format='%H %ai'",
         buildCommandLogByAuthor options)
 
 // ---------------------------------
